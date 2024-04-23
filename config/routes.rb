a532c94024a3dfeedc94846d28882ebb3b2deab1
tokushigeshuya <narihare1130@gmail.com>
2024-04-23 13:55:01 +0900

    [add]deviseの設定とルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,33 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+  devise_for :customers, controllers: {
+    session: 'customer/sessions'
+  }
+  devise_for :admins, controllers: {
+    session: 'admin/sessions',
+    registrations: 'admin/registrations'
+  }
+
   root to: 'pages#home'
 
   get '/up/', to: 'up#index', as: :up
   get '/up/databases', to: 'up#databases', as: :up_databases
-
-  # Sidekiq has a web dashboard which you can enable below. It's turned off by
-  # default because you very likely wouldn't want this to be available to
-  # everyone in production.
-  #
-  # Uncomment the 2 lines below to enable the dashboard WITHOUT authentication,
-  # but be careful because even anonymous web visitors will be able to see it!
-  # require "sidekiq/web"
-  # mount Sidekiq::Web => "/sidekiq"
-  #
-  # If you add Devise to this project and happen to have an admin? attribute
-  # on your user you can uncomment the 4 lines below to only allow access to
-  # the dashboard if you're an admin. Feel free to adjust things as needed.
-  # require "sidekiq/web"
-  # authenticate :user, lambda { |u| u.admin? } do
-  #   mount Sidekiq::Web => "/sidekiq"
-  # end
-
-  # Learn more about this file at: https://guides.rubyonrails.org/routing.html
 end
+
+# Sidekiq has a web dashboard which you can enable below. It's turned off by
+# default because you very likely wouldn't want this to be available to
+# everyone in production.
+#
+# Uncomment the 2 lines below to enable the dashboard WITHOUT authentication,
+# but be careful because even anonymous web visitors will be able to see it!
+# require "sidekiq/web"
+# mount Sidekiq::Web => "/sidekiq"
+#
+# If you add Devise to this project and happen to have an admin? attribute
+# on your user you can uncomment the 4 lines below to only allow access to
+# the dashboard if you're an admin. Feel free to adjust things as needed.
+# require "sidekiq/web"
+# authenticate :user, lambda { |u| u.admin? } do
+#   mount Sidekiq::Web => "/sidekiq"
+# end
+
+# Learn more about this file at: https://guides.rubyonrails.org/routing.html
