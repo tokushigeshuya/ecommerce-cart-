939e9aec9475b62d02c6294ce64cca2f2d32fc9d
tokushigeshuya <narihare1130@gmail.com>
2024-04-23 13:34:57 +0900

    [add] deviseの追加

---
+++ app/models/customer.rb
@@ -0,0 +1,18 @@
+class Customer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # バリデーション設定（name と status が空の場合）
+  with_options presence: true do
+    validates :name
+    validates :status
+  end
+
+  # enum
+  enum status: {
+    normal: 0, # 通常
+    with_drawn: 1, # 退会
+    banned: 2 # 停止
+  }
+end
